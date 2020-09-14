71d45b37201044e28f13e7f7da8781402b886d25
Adam Wilson <adamwilson1234@gmail.com>
2020-09-14 14:34:29 -0400

    Updates

---
+++ precache-manifest.349c48e99fcb415749015ecb2e6476c0.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e94bc20d3bd1e7f618e26aedf5d5c3a",
+    "url": "/scoppechio-test/index.html"
+  },
+  {
+    "revision": "56293dbe2b1e017b78e9",
+    "url": "/scoppechio-test/static/css/2.bf003f1a.chunk.css"
+  },
+  {
+    "revision": "e1f067b066ad44ad2619",
+    "url": "/scoppechio-test/static/css/main.0fbee40f.chunk.css"
+  },
+  {
+    "revision": "56293dbe2b1e017b78e9",
+    "url": "/scoppechio-test/static/js/2.7e350e62.chunk.js"
+  },
+  {
+    "revision": "b6840d1fa4cd3c3e88b7c215b28b2e9e",
+    "url": "/scoppechio-test/static/js/2.7e350e62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1f067b066ad44ad2619",
+    "url": "/scoppechio-test/static/js/main.4f4a8528.chunk.js"
+  },
+  {
+    "revision": "8aa32621da84e01d4cb7",
+    "url": "/scoppechio-test/static/js/runtime-main.01ad3717.js"
+  },
+  {
+    "revision": "c44c225806a44447f0fab2b76377f912",
+    "url": "/scoppechio-test/static/media/002-robot-head.c44c2258.svg"
+  },
+  {
+    "revision": "b44f4c591a41b93379370a0ae2a64135",
+    "url": "/scoppechio-test/static/media/FB_Icon_Red.b44f4c59.svg"
+  },
+  {
+    "revision": "c61077c39ac8e551405d7700b7312d3d",
+    "url": "/scoppechio-test/static/media/IG_Icon_Red.c61077c3.svg"
+  },
+  {
+    "revision": "a4302aad2447f61af81f28c1640c3a08",
+    "url": "/scoppechio-test/static/media/Twitter_Icon_Red.a4302aad.svg"
+  },
+  {
+    "revision": "48facbad595bb268396efa56b48f1c65",
+    "url": "/scoppechio-test/static/media/cancel_icon.48facbad.svg"
+  },
+  {
+    "revision": "674d9e57be0b5eb53767b84ea1bd2505",
+    "url": "/scoppechio-test/static/media/hamburger_icon.674d9e57.svg"
+  },
+  {
+    "revision": "10c4b458cfd9fa43019cc14b22ac7b1e",
+    "url": "/scoppechio-test/static/media/scoppechio-promo-video.10c4b458.mp4"
+  }
+]);
